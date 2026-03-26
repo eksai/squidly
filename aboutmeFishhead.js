74d7a1b821cc2666f17b565ad075be784e9ef628
eksai <100086420+eksai@users.noreply.github.com>
2026-03-26 09:48:33 -0700

    Add files via upload

--- aboutmeFishhead.js
+++ aboutmeFishhead.js
@@ -1,3 +1,3 @@
 alert("Hello, World!")
-aboutme="See also <a href="http://amazon.com/author/jeffmorin">amazon.com/author/jeffmorin</a>";
+aboutme="See also <a href="http:\/\/amazon.com\/author\/jeffmorin">amazon.com\/author\/jeffmorin<\/a>";
 
